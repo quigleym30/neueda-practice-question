963856d7e72c6fea32419db7355a134656e91ac4
quigleym30 <44317986+quigleym30@users.noreply.github.com>
2018-12-07 10:00:02 +0000

    times table completed

---
+++ PRACTICE-04-TimesTables/src/TimesTables.java
@@ -0,0 +1,14 @@
+
+public class TimesTables {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 12; i++) {
+			for (int j = 1; j <= 12; j++) {
+				System.out.println(i+" x "+j);
+			}
+		}
+
+	}
+
+}
