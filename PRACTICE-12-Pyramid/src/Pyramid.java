afcb49130d0c611246e5f9665cd444dd7866b77e
quigleym30 <44317986+quigleym30@users.noreply.github.com>
2018-12-07 12:42:24 +0000

    pyramid fix

--- PRACTICE-12-Pyramid/src/Pyramid.java
+++ PRACTICE-12-Pyramid/src/Pyramid.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class Pyramid {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in); 
-		 
-		 int rows = scan.nextInt(); 
-		 
-		 for(int i=0;i<rows;i++) {
-			 for(int j=0;j<rows-i;j++)
-				 System.out.print(" ");
-				
-		 for (int k=0;k<=i;k++) {
-			 System.out.print("# ");
+		
+        Scanner scan=new Scanner(System.in);
+        int row,temp,space,n;
+        n=scan.nextInt();
+        temp = n;
+        for ( row = 1 ; row <= n ; row++ )
+           {
+              for ( space = 1 ; space < temp ; space++ )
+                System.out.print(" ");
 
-		 }
-		 System.out.println(); 
-		 }
+              temp--;
+
+              for ( int i = 1 ; i <= 2*row - 1 ; i++ )
+                  System.out.print("*");
+
+              System.out.println("");
+           }
 	}
 }
 
