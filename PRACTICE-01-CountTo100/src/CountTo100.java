a1dc70b2e16a60a40682e03980a771aacb3859fa
quigleym30 <44317986+quigleym30@users.noreply.github.com>
2018-12-07 09:47:29 +0000

    count up completed

--- PRACTICE-01-CountTo100/src/CountTo100.java
+++ PRACTICE-01-CountTo100/src/CountTo100.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class CountTo100 {
 
 	public static void main(String[] args) {
 		
-		
-		for (int i=1;i<=100;i++)
+		 Scanner scan = new Scanner(System.in); 
+		 
+		 int max = scan.nextInt(); 
+		for (int i=1;i<=max;i++)
 		{
 			System.out.println(i);
 		}
